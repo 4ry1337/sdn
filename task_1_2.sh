b57c4df2db51caddbd1dd8163109c58ee3ab93a0
Rakhat Yskak <yskak.rakhat@gmail.com>
2025-09-07 12:26:23 +0500

    fix 3

--- task_1_2.sh
+++ task_1_2.sh
@@ -1,53 +1,35 @@
 #!/bin/bash
 
-ovs-ofctl add-flow s1 "priority=200,dl_type=0x800,nw_src=10.0.0.0/24,nw_dst=10.0.0.0/24,actions=normal"
+ovs-ofctl add-flow s1 priority=200,dl_type=0x800,nw_src=10.0.0.0/24,nw_dst=10.0.0.0/24,actions=normal
 
-ovs-ofctl add-flow s1 "priority=100,arp,nw_src=10.0.0.1,nw_dst=10.0.0.6,actions=drop"
-ovs-ofctl add-flow s1 "priority=100,arp,nw_dst=10.0.0.1,nw_src=10.0.0.6,actions=drop"
+# s1 lo:  s1-eth1:h1-eth0 s1-eth2:h2-eth0 s1-eth3:s2-eth2 s1-eth4:s3-eth3
+ovs-ofctl add-flow s1 arp,nw_dst=10.0.0.1,actions=output:1
+ovs-ofctl add-flow s1 arp,nw_dst=10.0.0.2,actions=output:2
+ovs-ofctl add-flow s1 arp,nw_dst=10.0.0.3,actions=output:3
+ovs-ofctl add-flow s1 arp,nw_dst=10.0.0.4,actions=output:4
+ovs-ofctl add-flow s1 arp,nw_dst=10.0.0.5,actions=output:4
 
-ovs-ofctl add-flow s1 "priority=100,arp,nw_src=10.0.0.2,nw_dst=10.0.0.6,actions=drop"
-ovs-ofctl add-flow s1 "priority=100,arp,nw_dst=10.0.0.2,nw_src=10.0.0.6,actions=drop"
+ovs-ofctl add-flow s2 priority=200,dl_type=0x800,nw_src=10.0.0.0/24,nw_dst=10.0.0.0/24,actions=normal
 
-ovs-ofctl add-flow s1 "priority=50,arp,nw_dst=10.0.0.1,actions=output:1"
-ovs-ofctl add-flow s1 "priority=50,arp,nw_dst=10.0.0.2,actions=output:2"
-ovs-ofctl add-flow s1 "priority=50,arp,nw_dst=10.0.0.3,actions=output:3"
-ovs-ofctl add-flow s1 "priority=50,arp,nw_dst=10.0.0.4,actions=output:4"
-ovs-ofctl add-flow s1 "priority=50,arp,nw_dst=10.0.0.5,actions=output:4"
+# s2 lo:  s2-eth1:h3-eth0 s2-eth2:s1-eth3 s2-eth3:s3-eth4 s2-eth4:s4-eth2
+ovs-ofctl add-flow s2 arp,nw_dst=10.0.0.3,actions=output:1
+ovs-ofctl add-flow s2 arp,nw_dst=10.0.0.1,actions=output:2
+ovs-ofctl add-flow s2 arp,nw_dst=10.0.0.2,actions=output:2
+ovs-ofctl add-flow s2 arp,nw_dst=10.0.0.4,actions=output:3
+ovs-ofctl add-flow s2 arp,nw_dst=10.0.0.5,actions=output:3
+ovs-ofctl add-flow s2 arp,nw_dst=10.0.0.6,actions=output:4
 
-ovs-ofctl add-flow s2 "priority=200,dl_type=0x800,nw_src=10.0.0.0/24,nw_dst=10.0.0.0/24,actions=normal"
+ovs-ofctl add-flow s3 priority=200,dl_type=0x800,nw_src=10.0.0.0/24,nw_dst=10.0.0.0/24,actions=normal
 
-ovs-ofctl add-flow s2 "priority=50,arp,nw_dst=10.0.0.1,actions=output:2"
-ovs-ofctl add-flow s2 "priority=50,arp,nw_dst=10.0.0.2,actions=output:2"
-ovs-ofctl add-flow s2 "priority=50,arp,nw_dst=10.0.0.3,actions=output:1"
-ovs-ofctl add-flow s2 "priority=50,arp,nw_dst=10.0.0.4,actions=output:3"
-ovs-ofctl add-flow s2 "priority=50,arp,nw_dst=10.0.0.5,actions=output:3"
-ovs-ofctl add-flow s2 "priority=50,arp,nw_dst=10.0.0.6,actions=output:4"
+# s3 lo:  s3-eth1:h4-eth0 s3-eth2:h5-eth0 s3-eth3:s1-eth4 s3-eth4:s2-eth3 s3-eth5:s4-eth3
+ovs-ofctl add-flow s3 arp,nw_dst=10.0.0.4,actions=output:1
+ovs-ofctl add-flow s3 arp,nw_dst=10.0.0.5,actions=output:2
+ovs-ofctl add-flow s3 arp,nw_dst=10.0.0.1,actions=output:3
+ovs-ofctl add-flow s3 arp,nw_dst=10.0.0.2,actions=output:3
+ovs-ofctl add-flow s3 arp,nw_dst=10.0.0.3,actions=output:4
 
-ovs-ofctl add-flow s3 "priority=200,dl_type=0x800,nw_src=10.0.0.0/24,nw_dst=10.0.0.0/24,actions=normal"
+ovs-ofctl add-flow s4 priority=200,dl_type=0x800,nw_src=10.0.0.0/24,nw_dst=10.0.0.0/24,actions=normal
 
-ovs-ofctl add-flow s3 "priority=100,arp,nw_src=10.0.0.4,nw_dst=10.0.0.6,actions=drop"
-ovs-ofctl add-flow s3 "priority=100,arp,nw_dst=10.0.0.4,nw_src=10.0.0.6,actions=drop"
-
-ovs-ofctl add-flow s3 "priority=100,arp,nw_src=10.0.0.5,nw_dst=10.0.0.6,actions=drop"
-ovs-ofctl add-flow s3 "priority=100,arp,nw_dst=10.0.0.5,nw_src=10.0.0.6,actions=drop"
-
-ovs-ofctl add-flow s3 "priority=50,arp,nw_dst=10.0.0.1,actions=output:3"
-ovs-ofctl add-flow s3 "priority=50,arp,nw_dst=10.0.0.2,actions=output:3"
-ovs-ofctl add-flow s3 "priority=50,arp,nw_dst=10.0.0.3,actions=output:4"
-ovs-ofctl add-flow s3 "priority=50,arp,nw_dst=10.0.0.4,actions=output:1"
-ovs-ofctl add-flow s3 "priority=50,arp,nw_dst=10.0.0.5,actions=output:2"
-
-ovs-ofctl add-flow s4 "priority=200,dl_type=0x800,nw_src=10.0.0.0/24,nw_dst=10.0.0.0/24,actions=normal"
-
-ovs-ofctl add-flow s4 "priority=100,arp,nw_src=10.0.0.6,nw_dst=10.0.0.1,actions=drop"
-ovs-ofctl add-flow s4 "priority=100,arp,nw_src=10.0.0.6,nw_dst=10.0.0.2,actions=drop"
-ovs-ofctl add-flow s4 "priority=100,arp,nw_src=10.0.0.6,nw_dst=10.0.0.4,actions=drop"
-ovs-ofctl add-flow s4 "priority=100,arp,nw_src=10.0.0.6,nw_dst=10.0.0.5,actions=drop"
-
-ovs-ofctl add-flow s4 "priority=100,arp,nw_dst=10.0.0.6,nw_src=10.0.0.1,actions=drop"
-ovs-ofctl add-flow s4 "priority=100,arp,nw_dst=10.0.0.6,nw_src=10.0.0.2,actions=drop"
-ovs-ofctl add-flow s4 "priority=100,arp,nw_dst=10.0.0.6,nw_src=10.0.0.4,actions=drop"
-ovs-ofctl add-flow s4 "priority=100,arp,nw_dst=10.0.0.6,nw_src=10.0.0.5,actions=drop"
-
-ovs-ofctl add-flow s4 "priority=50,arp,nw_dst=10.0.0.3,actions=output:2"
-ovs-ofctl add-flow s4 "priority=50,arp,nw_dst=10.0.0.6,actions=output:1"
+# s4 lo:  s4-eth1:h6-eth0 s4-eth2:s2-eth4 s4-eth3:s3-eth5
+ovs-ofctl add-flow s4 arp,nw_dst=10.0.0.3,actions=output:2
+ovs-ofctl add-flow s4 arp,nw_dst=10.0.0.6,actions=output:1
