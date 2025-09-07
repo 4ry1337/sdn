6c4fad3c8299087df15902b49347ce7c09a77e0d
Rakhat Yskak <yskak.rakhat@gmail.com>
2025-09-07 13:37:00 +0500

    fix 1

--- task_1_2.sh
+++ task_1_2.sh
@@ -29,4 +29,4 @@ ovs-ofctl add-flow s3 arp,nw_dst=10.0.0.3,actions=output:4
 ovs-ofctl add-flow s3 arp,nw_dst=10.0.0.4,actions=output:1
 ovs-ofctl add-flow s3 arp,nw_dst=10.0.0.5,actions=output:2
 # s4 lo:  s4-eth1:h6-eth0 s4-eth2:s2-eth4 s4-eth3:s3-eth5
-ovs-ofctl add-flow s4 priority=500,dl_type=0x800,nw_src=10.0.0.0/24,nw_dst=10.0.0.0/24,actions=normal
+# ovs-ofctl add-flow s4 priority=500,dl_type=0x800,nw_src=10.0.0.0/24,nw_dst=10.0.0.0/24,actions=normal
