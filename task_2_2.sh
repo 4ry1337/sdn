86ef9345891e4ee243caf53dacb881d737c1da0a
Rakhat Yskak <yskak.rakhat@gmail.com>
2025-09-07 20:17:45 +0500

    flood

--- task_2_2.sh
+++ task_2_2.sh
@@ -9,12 +9,11 @@ ovs-ofctl del-flows s6
 ovs-ofctl del-flows s7
 ovs-ofctl del-flows s8
 
-ovs-ofctl add-flow s1 actions=normal
-ovs-ofctl add-flow s2 actions=normal
-ovs-ofctl add-flow s3 actions=normal
-ovs-ofctl add-flow s4 actions=normal
-
-ovs-ofctl add-flow s5 actions=normal
-ovs-ofctl add-flow s6 actions=normal
-ovs-ofctl add-flow s7 actions=normal
-ovs-ofctl add-flow s8 actions=normal
+ovs-ofctl add-flow s1 actions=flood
+ovs-ofctl add-flow s2 actions=flood  
+ovs-ofctl add-flow s3 actions=flood
+ovs-ofctl add-flow s4 actions=flood
+ovs-ofctl add-flow s5 actions=flood
+ovs-ofctl add-flow s6 actions=flood
+ovs-ofctl add-flow s7 actions=flood
+ovs-ofctl add-flow s8 actions=flood
