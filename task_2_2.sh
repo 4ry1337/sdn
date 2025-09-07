6943ec43e2b303f355c73ecd7a3c3e69038e1195
Rakhat Yskak <yskak.rakhat@gmail.com>
2025-09-07 20:01:50 +0500

    try

--- task_2_2.sh
+++ task_2_2.sh
@@ -9,20 +9,77 @@ ovs-ofctl del-flows s6
 ovs-ofctl del-flows s7
 ovs-ofctl del-flows s8
 
-ovs-ofctl add-flow s1 priority=500,dl_type=0x800,nw_src=10.0.0.0/24,nw_dst=10.0.0.0/24,actions=normal
-ovs-ofctl add-flow s2 priority=500,dl_type=0x800,nw_src=10.0.0.0/24,nw_dst=10.0.0.0/24,actions=normal
-ovs-ofctl add-flow s3 priority=500,dl_type=0x800,nw_src=10.0.0.0/24,nw_dst=10.0.0.0/24,actions=normal
-ovs-ofctl add-flow s4 priority=500,dl_type=0x800,nw_src=10.0.0.0/24,nw_dst=10.0.0.0/24,actions=normal
-ovs-ofctl add-flow s5 priority=500,dl_type=0x800,nw_src=10.0.0.0/24,nw_dst=10.0.0.0/24,actions=normal
-ovs-ofctl add-flow s6 priority=500,dl_type=0x800,nw_src=10.0.0.0/24,nw_dst=10.0.0.0/24,actions=normal
-ovs-ofctl add-flow s7 priority=500,dl_type=0x800,nw_src=10.0.0.0/24,nw_dst=10.0.0.0/24,actions=normal
-ovs-ofctl add-flow s8 priority=500,dl_type=0x800,nw_src=10.0.0.0/24,nw_dst=10.0.0.0/24,actions=normal
+ovs-ofctl add-flow s1 priority=100,dl_type=0x806,actions=normal  # ARP
+ovs-ofctl add-flow s2 priority=100,dl_type=0x806,actions=normal
+ovs-ofctl add-flow s3 priority=100,dl_type=0x806,actions=normal
+ovs-ofctl add-flow s4 priority=100,dl_type=0x806,actions=normal
 
-ovs-ofctl add-flow s1 priority=500,dl_type=0x806,actions=normal
-ovs-ofctl add-flow s2 priority=500,dl_type=0x806,actions=normal
-ovs-ofctl add-flow s3 priority=500,dl_type=0x806,actions=normal
-ovs-ofctl add-flow s4 priority=500,dl_type=0x806,actions=normal
-ovs-ofctl add-flow s5 priority=500,dl_type=0x806,actions=normal
-ovs-ofctl add-flow s6 priority=500,dl_type=0x806,actions=normal
-ovs-ofctl add-flow s7 priority=500,dl_type=0x806,actions=normal
-ovs-ofctl add-flow s8 priority=500,dl_type=0x806,actions=normal
+ovs-ofctl add-flow s1 priority=200,dl_type=0x800,nw_dst=10.0.0.1,actions=normal
+ovs-ofctl add-flow s1 priority=200,dl_type=0x800,nw_dst=10.0.0.2,actions=normal
+ovs-ofctl add-flow s1 priority=200,dl_type=0x800,nw_dst=10.0.0.3,actions=normal
+ovs-ofctl add-flow s2 priority=200,dl_type=0x800,nw_dst=10.0.0.1,actions=normal
+ovs-ofctl add-flow s2 priority=200,dl_type=0x800,nw_dst=10.0.0.2,actions=normal
+ovs-ofctl add-flow s2 priority=200,dl_type=0x800,nw_dst=10.0.0.3,actions=normal
+ovs-ofctl add-flow s3 priority=200,dl_type=0x800,nw_dst=10.0.0.1,actions=normal
+ovs-ofctl add-flow s3 priority=200,dl_type=0x800,nw_dst=10.0.0.2,actions=normal
+ovs-ofctl add-flow s3 priority=200,dl_type=0x800,nw_dst=10.0.0.3,actions=normal
+ovs-ofctl add-flow s4 priority=200,dl_type=0x800,nw_dst=10.0.0.1,actions=normal
+ovs-ofctl add-flow s4 priority=200,dl_type=0x800,nw_dst=10.0.0.2,actions=normal
+ovs-ofctl add-flow s4 priority=200,dl_type=0x800,nw_dst=10.0.0.3,actions=normal
+
+ovs-ofctl add-flow s1 priority=300,dl_type=0x800,nw_dst=10.0.0.4,actions=output:2  # to s2
+ovs-ofctl add-flow s1 priority=300,dl_type=0x800,nw_dst=10.0.0.5,actions=output:2  # to s2
+ovs-ofctl add-flow s1 priority=300,dl_type=0x800,nw_dst=10.0.0.6,actions=output:2  # to s2
+ovs-ofctl add-flow s2 priority=300,dl_type=0x800,nw_dst=10.0.0.4,actions=output:4  # to s6
+ovs-ofctl add-flow s2 priority=300,dl_type=0x800,nw_dst=10.0.0.5,actions=output:4  # to s6
+ovs-ofctl add-flow s2 priority=300,dl_type=0x800,nw_dst=10.0.0.6,actions=output:4  # to s6
+ovs-ofctl add-flow s3 priority=300,dl_type=0x800,nw_dst=10.0.0.4,actions=output:4  # to s7
+ovs-ofctl add-flow s3 priority=300,dl_type=0x800,nw_dst=10.0.0.5,actions=output:4  # to s7
+ovs-ofctl add-flow s3 priority=300,dl_type=0x800,nw_dst=10.0.0.6,actions=output:4  # to s7
+ovs-ofctl add-flow s4 priority=300,dl_type=0x800,nw_dst=10.0.0.4,actions=output:1  # via s2
+ovs-ofctl add-flow s4 priority=300,dl_type=0x800,nw_dst=10.0.0.5,actions=output:1  # via s2
+ovs-ofctl add-flow s4 priority=300,dl_type=0x800,nw_dst=10.0.0.6,actions=output:1  # via s2
+
+ovs-ofctl add-flow s5 priority=100,dl_type=0x806,actions=normal  # ARP
+ovs-ofctl add-flow s6 priority=100,dl_type=0x806,actions=normal
+ovs-ofctl add-flow s7 priority=100,dl_type=0x806,actions=normal
+ovs-ofctl add-flow s8 priority=100,dl_type=0x806,actions=normal
+
+ovs-ofctl add-flow s5 priority=200,dl_type=0x800,nw_dst=10.0.0.4,actions=normal
+ovs-ofctl add-flow s5 priority=200,dl_type=0x800,nw_dst=10.0.0.5,actions=normal
+ovs-ofctl add-flow s5 priority=200,dl_type=0x800,nw_dst=10.0.0.6,actions=normal
+ovs-ofctl add-flow s6 priority=200,dl_type=0x800,nw_dst=10.0.0.4,actions=normal
+ovs-ofctl add-flow s6 priority=200,dl_type=0x800,nw_dst=10.0.0.5,actions=normal
+ovs-ofctl add-flow s6 priority=200,dl_type=0x800,nw_dst=10.0.0.6,actions=normal
+ovs-ofctl add-flow s7 priority=200,dl_type=0x800,nw_dst=10.0.0.4,actions=normal
+ovs-ofctl add-flow s7 priority=200,dl_type=0x800,nw_dst=10.0.0.5,actions=normal
+ovs-ofctl add-flow s7 priority=200,dl_type=0x800,nw_dst=10.0.0.6,actions=normal
+ovs-ofctl add-flow s8 priority=200,dl_type=0x800,nw_dst=10.0.0.4,actions=normal
+ovs-ofctl add-flow s8 priority=200,dl_type=0x800,nw_dst=10.0.0.5,actions=normal
+ovs-ofctl add-flow s8 priority=200,dl_type=0x800,nw_dst=10.0.0.6,actions=normal
+
+ovs-ofctl add-flow s5 priority=300,dl_type=0x800,nw_dst=10.0.0.1,actions=output:1  # via s6
+ovs-ofctl add-flow s5 priority=300,dl_type=0x800,nw_dst=10.0.0.2,actions=output:1  # via s6
+ovs-ofctl add-flow s5 priority=300,dl_type=0x800,nw_dst=10.0.0.3,actions=output:1  # via s6
+ovs-ofctl add-flow s6 priority=300,dl_type=0x800,nw_dst=10.0.0.1,actions=output:4  # to s2
+ovs-ofctl add-flow s6 priority=300,dl_type=0x800,nw_dst=10.0.0.2,actions=output:4  # to s2
+ovs-ofctl add-flow s6 priority=300,dl_type=0x800,nw_dst=10.0.0.3,actions=output:4  # to s2
+ovs-ofctl add-flow s7 priority=300,dl_type=0x800,nw_dst=10.0.0.1,actions=output:3  # to s3
+ovs-ofctl add-flow s7 priority=300,dl_type=0x800,nw_dst=10.0.0.2,actions=output:3  # to s3
+ovs-ofctl add-flow s7 priority=300,dl_type=0x800,nw_dst=10.0.0.3,actions=output:3  # to s3
+ovs-ofctl add-flow s8 priority=300,dl_type=0x800,nw_dst=10.0.0.1,actions=output:3  # via s6
+ovs-ofctl add-flow s8 priority=300,dl_type=0x800,nw_dst=10.0.0.2,actions=output:3  # via s6
+ovs-ofctl add-flow s8 priority=300,dl_type=0x800,nw_dst=10.0.0.3,actions=output:3  # via s6
+
+ovs-ofctl add-flow s2 priority=400,dl_type=0x806,arp_tpa=10.0.0.4,actions=output:4
+ovs-ofctl add-flow s2 priority=400,dl_type=0x806,arp_tpa=10.0.0.5,actions=output:4
+ovs-ofctl add-flow s2 priority=400,dl_type=0x806,arp_tpa=10.0.0.6,actions=output:4
+ovs-ofctl add-flow s3 priority=400,dl_type=0x806,arp_tpa=10.0.0.4,actions=output:4
+ovs-ofctl add-flow s3 priority=400,dl_type=0x806,arp_tpa=10.0.0.5,actions=output:4
+ovs-ofctl add-flow s3 priority=400,dl_type=0x806,arp_tpa=10.0.0.6,actions=output:4
+ovs-ofctl add-flow s6 priority=400,dl_type=0x806,arp_tpa=10.0.0.1,actions=output:4
+ovs-ofctl add-flow s6 priority=400,dl_type=0x806,arp_tpa=10.0.0.2,actions=output:4
+ovs-ofctl add-flow s6 priority=400,dl_type=0x806,arp_tpa=10.0.0.3,actions=output:4
+ovs-ofctl add-flow s7 priority=400,dl_type=0x806,arp_tpa=10.0.0.1,actions=output:3
+ovs-ofctl add-flow s7 priority=400,dl_type=0x806,arp_tpa=10.0.0.2,actions=output:3
+ovs-ofctl add-flow s7 priority=400,dl_type=0x806,arp_tpa=10.0.0.3,actions=output:3
