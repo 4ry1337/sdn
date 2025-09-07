d6f15f3ebcaea5628960cdbabb1cb53d3efdb1af
Rakhat Yskak <yskak.rakhat@gmail.com>
2025-09-08 03:31:55 +0500

    various options

--- Rakhat_Yskak_2.py
+++ Rakhat_Yskak_2.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
-
 from mininet.net import Mininet
 from mininet.node import OVSController, OVSSwitch
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info
+from mininet.link import TCLink
 
 def topo():
     net = Mininet( controller=OVSController, switch=OVSSwitch,
-                   waitConnected=True )
-
+                   waitConnected=True, link=TCLink, autoStaticArp=True,
+                  autoSetMacs=True )
+    
     info( "Creating controllers\n" )
-    c1 = net.addController('c1', controller=OVSController, port=6633)
-    c2 = net.addController('c2', controller=OVSController, port=6634)
-
+    c1 = net.addController( 'c1', port=6633 )
+    c2 = net.addController( 'c2', port=6634 )
+    
     info( "Creating switches\n" )
     s1 = net.addSwitch( 's1' )
     s2 = net.addSwitch( 's2' )
@@ -22,15 +23,15 @@ def topo():
     s6 = net.addSwitch( 's6' )
     s7 = net.addSwitch( 's7' )
     s8 = net.addSwitch( 's8' )
-
-    info( "Creating hosts\n" )
+    
+    info( "Creating hosts with fixed MACs and IPs\n" )
     h1 = net.addHost( 'h1' )
     h2 = net.addHost( 'h2' )
     h3 = net.addHost( 'h3' )
     h4 = net.addHost( 'h4' )
     h5 = net.addHost( 'h5' )
     h6 = net.addHost( 'h6' )
-
+    
     info( "Creating links\n" )
     net.addLink( s1, h1 )
     net.addLink( s2, h2 )
@@ -39,7 +40,6 @@ def topo():
     net.addLink( s1, s3 )
     net.addLink( s4, s2 )
     net.addLink( s4, s3 )
-
     net.addLink( s6, h4 )
     net.addLink( s8, h5 )
     net.addLink( s8, h6 )
@@ -47,12 +47,12 @@ def topo():
     net.addLink( s5, s7 )
     net.addLink( s8, s6 )
     net.addLink( s8, s7 )
-
-    net.addLink(s2, s6)
-    net.addLink(s3, s7)
-
+    net.addLink( s2, s6 )
+    net.addLink( s3, s7 )
+    
     info( "Starting network\n" )
     net.build()
+    
     c1.start()
     c2.start()
     s1.start( [ c1 ] )
@@ -63,16 +63,15 @@ def topo():
     s6.start( [ c2 ] )
     s7.start( [ c2 ] )
     s8.start( [ c2 ] )
-
+    
     info( "Testing network\n" )
     net.pingAll()
-
+    
     info( "Running CLI\n" )
     CLI( net )
-
+    
     info( "Stopping network\n" )
     net.stop()
-
 
 if __name__ == '__main__':
     setLogLevel( 'info' )  # for CLI output
