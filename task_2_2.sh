543fc91bf7350fab019865d7e8bef7c3f6bd4020
Rakhat Yskak <yskak.rakhat@gmail.com>
2025-09-08 03:34:16 +0500

    flow rules between domains

--- task_2_2.sh
+++ task_2_2.sh
@@ -1,36 +1,20 @@
 #!/bin/bash
 
-ovs-ofctl add-flow s1 priority=1000,arp,actions=flood
-ovs-ofctl add-flow s2 priority=1000,arp,actions=flood 
-ovs-ofctl add-flow s3 priority=1000,arp,actions=flood
-ovs-ofctl add-flow s4 priority=1000,arp,actions=flood
-ovs-ofctl add-flow s5 priority=1000,arp,actions=flood
-ovs-ofctl add-flow s6 priority=1000,arp,actions=flood
-ovs-ofctl add-flow s7 priority=1000,arp,actions=flood
-ovs-ofctl add-flow s8 priority=1000,arp,actions=flood
+ovs-ofctl add-flow s2 "dl_dst=00:00:00:00:00:04,actions=output:3"  # h4 via s6
+ovs-ofctl add-flow s2 "dl_dst=00:00:00:00:00:05,actions=output:3"  # h5 via s6
+ovs-ofctl add-flow s2 "dl_dst=00:00:00:00:00:06,actions=output:3"  # h6 via s6
 
-ovs-ofctl add-flow s2 priority=2000,ip,nw_dst=10.0.0.4,actions=output:3  # h4 via s6
-ovs-ofctl add-flow s2 priority=2000,ip,nw_dst=10.0.0.5,actions=output:3  # h5 via s6  
-ovs-ofctl add-flow s2 priority=2000,ip,nw_dst=10.0.0.6,actions=output:3  # h6 via s6
+ovs-ofctl add-flow s3 "dl_dst=00:00:00:00:00:04,actions=output:3"  # h4 via s7
+ovs-ofctl add-flow s3 "dl_dst=00:00:00:00:00:05,actions=output:3"  # h5 via s7  
+ovs-ofctl add-flow s3 "dl_dst=00:00:00:00:00:06,actions=output:3"  # h6 via s7
 
-ovs-ofctl add-flow s6 priority=2000,ip,nw_dst=10.0.0.1,actions=output:3  # h1 via s2
-ovs-ofctl add-flow s6 priority=2000,ip,nw_dst=10.0.0.2,actions=output:3  # h2 via s2
-ovs-ofctl add-flow s6 priority=2000,ip,nw_dst=10.0.0.3,actions=output:3  # h3 via s2
+ovs-ofctl add-flow s6 "dl_dst=00:00:00:00:00:01,actions=output:3"  # h1 via s2
+ovs-ofctl add-flow s6 "dl_dst=00:00:00:00:00:02,actions=output:3"  # h2 via s2
+ovs-ofctl add-flow s6 "dl_dst=00:00:00:00:00:03,actions=output:3"  # h3 via s2
 
-ovs-ofctl add-flow s3 priority=2000,ip,nw_dst=10.0.0.4,actions=output:3  # h4 via s7
-ovs-ofctl add-flow s3 priority=2000,ip,nw_dst=10.0.0.5,actions=output:3  # h5 via s7
-ovs-ofctl add-flow s3 priority=2000,ip,nw_dst=10.0.0.6,actions=output:3  # h6 via s7
+ovs-ofctl add-flow s7 "dl_dst=00:00:00:00:00:01,actions=output:2"  # h1 via s3
+ovs-ofctl add-flow s7 "dl_dst=00:00:00:00:00:02,actions=output:2"  # h2 via s3
+ovs-ofctl add-flow s7 "dl_dst=00:00:00:00:00:03,actions=output:2"  # h3 via s3
 
-ovs-ofctl add-flow s7 priority=2000,ip,nw_dst=10.0.0.1,actions=output:3  # h1 via s3
-ovs-ofctl add-flow s7 priority=2000,ip,nw_dst=10.0.0.2,actions=output:3  # h2 via s3
-ovs-ofctl add-flow s7 priority=2000,ip,nw_dst=10.0.0.3,actions=output:3  # h3 via s3
 
-ovs-ofctl add-flow s1 priority=500,actions=flood
-ovs-ofctl add-flow s2 priority=500,actions=flood 
-ovs-ofctl add-flow s3 priority=500,actions=flood
-ovs-ofctl add-flow s4 priority=500,actions=flood
-
-ovs-ofctl add-flow s5 priority=500,actions=flood
-ovs-ofctl add-flow s6 priority=500,actions=flood
-ovs-ofctl add-flow s7 priority=500,actions=flood 
-ovs-ofctl add-flow s8 priority=500,actions=flood
+echo "Flow rules added successfully!"
