3e383974f93723eef18e479ca09d4cf0d1210e0c
Rakhat Yskak <yskak.rakhat@gmail.com>
2025-09-07 15:10:12 +0500

    arp for each host

--- task_1_2.sh
+++ task_1_2.sh
@@ -13,9 +13,11 @@ ovs-ofctl add-flow s2 priority=500,dl_type=0x800,nw_src=10.0.0.0/24,nw_dst=10.0.
 # s4 lo:  s4-eth1:h6-eth0 s4-eth2:s2-eth4 s4-eth3:s3-eth5
 ovs-ofctl add-flow s1 arp,nw_dst=10.0.0.1,actions=output:1
 ovs-ofctl add-flow s1 arp,nw_dst=10.0.0.2,actions=output:2
-ovs-ofctl add-flow s1 arp,nw_dst=10.0.0.3,actions=output:3
 
-ovs-ofctl add-flow s2 arp,nw_dst=10.0.0.1,actions=output:2
-ovs-ofctl add-flow s2 arp,nw_dst=10.0.0.2,actions=output:2
 ovs-ofctl add-flow s2 arp,nw_dst=10.0.0.3,actions=output:1
+
+ovs-ofctl add-flow s3 arp,nw_dst=10.0.0.4,actions=output:1
+ovs-ofctl add-flow s3 arp,nw_dst=10.0.0.5,actions=output:2
+
+ovs-ofctl add-flow s4 arp,nw_dst=10.0.0.6,actions=output:1
 # ovs-ofctl add-flow s4 priority=500,dl_type=0x800,nw_src=10.0.0.0/24,nw_dst=10.0.0.0/24,actions=normal
