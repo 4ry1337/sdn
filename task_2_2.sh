86fa91c3afce418e5514ea1bb68e962ac25119f7
Rakhat Yskak <yskak.rakhat@gmail.com>
2025-09-07 20:11:25 +0500

    try legacy

--- task_2_2.sh
+++ task_2_2.sh
@@ -8,3 +8,13 @@ ovs-ofctl del-flows s5
 ovs-ofctl del-flows s6
 ovs-ofctl del-flows s7
 ovs-ofctl del-flows s8
+
+ovs-ofctl add-flow s1 actions=normal
+ovs-ofctl add-flow s2 actions=normal
+ovs-ofctl add-flow s3 actions=normal
+ovs-ofctl add-flow s4 actions=normal
+
+ovs-ofctl add-flow s5 actions=normal
+ovs-ofctl add-flow s6 actions=normal
+ovs-ofctl add-flow s7 actions=normal
+ovs-ofctl add-flow s8 actions=normal
