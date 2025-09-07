736ae55ec4395e3fd20569597b1c328c0a18fa54
Rakhat Yskak <yskak.rakhat@gmail.com>
2025-09-07 19:36:02 +0500

    try ovs commands

--- task_2_2.sh
+++ task_2_2.sh
@@ -9,19 +9,20 @@ ovs-ofctl del-flows s6
 ovs-ofctl del-flows s7
 ovs-ofctl del-flows s8
 
-ovs-ofctl add-flow s2 "ip,nw_dst=10.0.0.4,actions=output:4"
-ovs-ofctl add-flow s2 "ip,nw_dst=10.0.0.5,actions=output:4"
-ovs-ofctl add-flow s2 "ip,nw_dst=10.0.0.6,actions=output:4"
-ovs-ofctl add-flow s3 "ip,nw_dst=10.0.0.4,actions=output:4"
-ovs-ofctl add-flow s3 "ip,nw_dst=10.0.0.5,actions=output:4"
-ovs-ofctl add-flow s3 "ip,nw_dst=10.0.0.6,actions=output:4"
-ovs-ofctl add-flow s6 "ip,nw_dst=10.0.0.1,actions=output:4"
-ovs-ofctl add-flow s6 "ip,nw_dst=10.0.0.2,actions=output:4"
-ovs-ofctl add-flow s6 "ip,nw_dst=10.0.0.3,actions=output:4"
-ovs-ofctl add-flow s7 "ip,nw_dst=10.0.0.1,actions=output:3"
-ovs-ofctl add-flow s7 "ip,nw_dst=10.0.0.2,actions=output:3"
-ovs-ofctl add-flow s7 "ip,nw_dst=10.0.0.3,actions=output:3"
-ovs-ofctl add-flow s2 "arp,actions=flood"
-ovs-ofctl add-flow s3 "arp,actions=flood"
-ovs-ofctl add-flow s6 "arp,actions=flood"
-ovs-ofctl add-flow s7 "arp,actions=flood"
+ovs-ofctl add-flow s1 priority=500,dl_type=0x800,nw_src=10.0.0.0/24,nw_dst=10.0.0.0/24,actions=normal
+ovs-ofctl add-flow s2 priority=500,dl_type=0x800,nw_src=10.0.0.0/24,nw_dst=10.0.0.0/24,actions=normal
+ovs-ofctl add-flow s3 priority=500,dl_type=0x800,nw_src=10.0.0.0/24,nw_dst=10.0.0.0/24,actions=normal
+ovs-ofctl add-flow s4 priority=500,dl_type=0x800,nw_src=10.0.0.0/24,nw_dst=10.0.0.0/24,actions=normal
+ovs-ofctl add-flow s5 priority=500,dl_type=0x800,nw_src=10.0.0.0/24,nw_dst=10.0.0.0/24,actions=normal
+ovs-ofctl add-flow s6 priority=500,dl_type=0x800,nw_src=10.0.0.0/24,nw_dst=10.0.0.0/24,actions=normal
+ovs-ofctl add-flow s7 priority=500,dl_type=0x800,nw_src=10.0.0.0/24,nw_dst=10.0.0.0/24,actions=normal
+ovs-ofctl add-flow s8 priority=500,dl_type=0x800,nw_src=10.0.0.0/24,nw_dst=10.0.0.0/24,actions=normal
+
+ovs-ofctl add-flow s1 priority=500,dl_type=0x806,actions=normal
+ovs-ofctl add-flow s2 priority=500,dl_type=0x806,actions=normal
+ovs-ofctl add-flow s3 priority=500,dl_type=0x806,actions=normal
+ovs-ofctl add-flow s4 priority=500,dl_type=0x806,actions=normal
+ovs-ofctl add-flow s5 priority=500,dl_type=0x806,actions=normal
+ovs-ofctl add-flow s6 priority=500,dl_type=0x806,actions=normal
+ovs-ofctl add-flow s7 priority=500,dl_type=0x806,actions=normal
+ovs-ofctl add-flow s8 priority=500,dl_type=0x806,actions=normal
