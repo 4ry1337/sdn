abba8ec6b2d7b399fc2613c1b436d4759ab99106
Rakhat Yskak <yskak.rakhat@gmail.com>
2025-09-07 13:53:33 +0500

    fix

--- task_1_2.sh
+++ task_1_2.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "Installing flow rules for Problem 1..."
-
 ovs-ofctl del-flows s1
 ovs-ofctl del-flows s2
 ovs-ofctl del-flows s3
@@ -17,20 +15,19 @@ ovs-ofctl add-flow s3 priority=500,dl_type=0x800,nw_src=10.0.0.0/24,nw_dst=10.0.
 # s4 lo:  s4-eth1:h6-eth0 s4-eth2:s2-eth4 s4-eth3:s3-eth5
 ovs-ofctl add-flow s1 arp,nw_dst=10.0.0.1,actions=output:1
 ovs-ofctl add-flow s1 arp,nw_dst=10.0.0.2,actions=output:2
-ovs-ofctl add-flow s2 arp,nw_dst=10.0.0.3,actions=output:1
-ovs-ofctl add-flow s3 arp,nw_dst=10.0.0.4,actions=output:1
-ovs-ofctl add-flow s3 arp,nw_dst=10.0.0.5,actions=output:2
-
 ovs-ofctl add-flow s1 arp,nw_dst=10.0.0.3,actions=output:3
 ovs-ofctl add-flow s1 arp,nw_dst=10.0.0.4,actions=output:4
 ovs-ofctl add-flow s1 arp,nw_dst=10.0.0.5,actions=output:4
 
 ovs-ofctl add-flow s2 arp,nw_dst=10.0.0.1,actions=output:2
 ovs-ofctl add-flow s2 arp,nw_dst=10.0.0.2,actions=output:2
+ovs-ofctl add-flow s2 arp,nw_dst=10.0.0.3,actions=output:1
 ovs-ofctl add-flow s2 arp,nw_dst=10.0.0.4,actions=output:3
 ovs-ofctl add-flow s2 arp,nw_dst=10.0.0.5,actions=output:3
 
 ovs-ofctl add-flow s3 arp,nw_dst=10.0.0.1,actions=output:3
 ovs-ofctl add-flow s3 arp,nw_dst=10.0.0.2,actions=output:3
 ovs-ofctl add-flow s3 arp,nw_dst=10.0.0.3,actions=output:4
+ovs-ofctl add-flow s3 arp,nw_dst=10.0.0.4,actions=output:1
+ovs-ofctl add-flow s3 arp,nw_dst=10.0.0.5,actions=output:2
 # ovs-ofctl add-flow s4 priority=500,dl_type=0x800,nw_src=10.0.0.0/24,nw_dst=10.0.0.0/24,actions=normal
