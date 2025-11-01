2a5bd21b843fcda97adf68e9281944ecfc2a5311
Rakhat Yskak <yskak.rakhat@gmail.com>
2025-11-01 23:25:03 +0500

    [fix] try static flows

--- Assignment2/flows.sh
+++ Assignment2/flows.sh
@@ -1,57 +1,48 @@
 #!/bin/bash
 
-# Delete all existing flows from switches and APs
-sudo ovs-ofctl del-flows c1_s1
-sudo ovs-ofctl del-flows c1_s2
-sudo ovs-ofctl del-flows c1_ap1
+# Domain 1 - Allow all traffic to flood initially
+# This makes the learning switch actually learn
 
-sudo ovs-ofctl del-flows c2_s1
-sudo ovs-ofctl del-flows c2_s2
-sudo ovs-ofctl del-flows c2_ap1
+# Clear existing flows first
+curl -X DELETE http://localhost:8080/wm/staticentrypusher/clear/00:00:00:00:00:00:00:01/json
+curl -X DELETE http://localhost:8080/wm/staticentrypusher/clear/00:00:00:00:00:00:00:02/json
 
-sudo ovs-ofctl del-flows c3_s1
-sudo ovs-ofctl del-flows c3_s2
-sudo ovs-ofctl del-flows c3_ap1
+# On c1_s1 - flood ARP and IP packets
+curl -X POST -d '{
+    "switch":"00:00:00:00:00:00:00:01",
+    "name":"flood_arp",
+    "priority":"100",
+    "eth_type":"0x0806",
+    "active":"true",
+    "actions":"output=flood"
+}' http://localhost:8080/wm/staticentrypusher/json
 
-# mininet-wifi> net
-# c1_h1 c1_h1-eth0:c1_s1-eth3
-# c1_h2 c1_h2-eth0:c1_s1-eth4
-# c1_h3 c1_h3-eth0:c1_s2-eth2
-# c1_h4 c1_h4-eth0:c1_s2-eth3
-# c2_h1 c2_h1-eth0:c2_s1-eth3
-# c2_h2 c2_h2-eth0:c2_s1-eth4
-# c2_h3 c2_h3-eth0:c2_s2-eth2
-# c2_h4 c2_h4-eth0:c2_s2-eth3
-# c3_h1 c3_h1-eth0:c3_s1-eth3
-# c3_h2 c3_h2-eth0:c3_s1-eth4
-# c3_h3 c3_h3-eth0:c3_s2-eth2
-# c3_h4 c3_h4-eth0:c3_s2-eth3
-# c1_s1 lo:  c1_s1-eth1:c1_ap1-eth1 c1_s1-eth2:c1_s2-eth1 c1_s1-eth3:c1_h1-eth0 c1_s1-eth4:c1_h2-eth0 c1_s1-eth5:c2_s1-eth5
-# c1_s2 lo:  c1_s2-eth1:c1_s1-eth2 c1_s2-eth2:c1_h3-eth0 c1_s2-eth3:c1_h4-eth0 c1_s2-eth4:c2_s2-eth4 c1_s2-eth5:c3_s1-eth6
-# c2_s1 lo:  c2_s1-eth1:c2_ap1-eth1 c2_s1-eth2:c2_s2-eth1 c2_s1-eth3:c2_h1-eth0 c2_s1-eth4:c2_h2-eth0 c2_s1-eth5:c1_s1-eth5 c2_s1-eth6:c3_s1-eth5
-# c2_s2 lo:  c2_s2-eth1:c2_s1-eth2 c2_s2-eth2:c2_h3-eth0 c2_s2-eth3:c2_h4-eth0 c2_s2-eth4:c1_s2-eth4 c2_s2-eth5:c3_s2-eth4
-# c3_s1 lo:  c3_s1-eth1:c3_ap1-eth1 c3_s1-eth2:c3_s2-eth1 c3_s1-eth3:c3_h1-eth0 c3_s1-eth4:c3_h2-eth0 c3_s1-eth5:c2_s1-eth6 c3_s1-eth6:c1_s2-eth5
-# c3_s2 lo:  c3_s2-eth1:c3_s1-eth2 c3_s2-eth2:c3_h3-eth0 c3_s2-eth3:c3_h4-eth0 c3_s2-eth4:c2_s2-eth5
-# c6653
-# c6654
-# c6655
-# sta1 sta1-wlan0:wifi
-# sta2 sta2-wlan0:wifi
-# sta3 sta3-wlan0:wifi
-# c1_ap1 lo:  c1_ap1-eth1:c1_s1-eth1 c1_ap1-wlan2:wifi c1_ap1-wlan3:wifi
-# c2_ap1 lo:  c2_ap1-eth1:c2_s1-eth1 c2_ap1-wlan2:wifi c2_ap1-wlan3:wifi
-# c3_ap1 lo:  c3_ap1-eth1:c3_s1-eth1 c3_ap1-wlan2:wifi c3_ap1-wlan3:wifi
+curl -X POST -d '{
+    "switch":"00:00:00:00:00:00:00:01",
+    "name":"flood_ip",
+    "priority":"100",
+    "eth_type":"0x0800",
+    "active":"true",
+    "actions":"output=flood"
+}' http://localhost:8080/wm/staticentrypusher/json
 
-# Add flood rule to each (default action: flood all unmatched packets)
-# Use -O OpenFlow13 to ensure compatibility
-sudo ovs-ofctl add-flow c1_s1 priority=0,actions=flood
-sudo ovs-ofctl add-flow c1_s2 priority=0,actions=flood
-sudo ovs-ofctl add-flow c1_ap1 priority=0,actions=flood
+# On c1_s2 - flood ARP and IP packets
+curl -X POST -d '{
+    "switch":"00:00:00:00:00:00:00:02",
+    "name":"flood_arp",
+    "priority":"100",
+    "eth_type":"0x0806",
+    "active":"true",
+    "actions":"output=flood"
+}' http://localhost:8080/wm/staticentrypusher/json
 
-sudo ovs-ofctl add-flow c2_s1 priority=0,actions=flood
-sudo ovs-ofctl add-flow c2_s2 priority=0,actions=flood
-sudo ovs-ofctl add-flow c2_ap1 priority=0,actions=flood
+curl -X POST -d '{
+    "switch":"00:00:00:00:00:00:00:02",
+    "name":"flood_ip",
+    "priority":"100",
+    "eth_type":"0x0800",
+    "active":"true",
+    "actions":"output=flood"
+}' http://localhost:8080/wm/staticentrypusher/json
 
-sudo ovs-ofctl add-flow c3_s1 priority=0,actions=flood
-sudo ovs-ofctl add-flow c3_s2 priority=0,actions=flood
-sudo ovs-ofctl add-flow c3_ap1 priority=0,actions=flood
+echo "Flows installed - test with: c1_h1 ping c1_h3"
