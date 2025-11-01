03dd022e76c37f686ef8f48383ea4bd062f00562
Rakhat Yskak <yskak.rakhat@gmail.com>
2025-11-01 11:18:42 +0500

    [fix] static flows

---
+++ Assignment2/flows.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Delete all existing flows from switches and APs
+ovs-ofctl del-flows c1_s1
+ovs-ofctl del-flows c1_s2
+ovs-ofctl del-flows c1_ap1
+
+ovs-ofctl del-flows c2_s1
+ovs-ofctl del-flows c2_s2
+ovs-ofctl del-flows c2_ap1
+
+ovs-ofctl del-flows c3_s1
+ovs-ofctl del-flows c3_s2
+ovs-ofctl del-flows c3_ap1
+
+# mininet-wifi> net
+# c1_h1 c1_h1-eth0:c1_s1-eth3
+# c1_h2 c1_h2-eth0:c1_s1-eth4
+# c1_h3 c1_h3-eth0:c1_s2-eth2
+# c1_h4 c1_h4-eth0:c1_s2-eth3
+# c2_h1 c2_h1-eth0:c2_s1-eth3
+# c2_h2 c2_h2-eth0:c2_s1-eth4
+# c2_h3 c2_h3-eth0:c2_s2-eth2
+# c2_h4 c2_h4-eth0:c2_s2-eth3
+# c3_h1 c3_h1-eth0:c3_s1-eth3
+# c3_h2 c3_h2-eth0:c3_s1-eth4
+# c3_h3 c3_h3-eth0:c3_s2-eth2
+# c3_h4 c3_h4-eth0:c3_s2-eth3
+# c1_s1 lo:  c1_s1-eth1:c1_ap1-eth1 c1_s1-eth2:c1_s2-eth1 c1_s1-eth3:c1_h1-eth0 c1_s1-eth4:c1_h2-eth0 c1_s1-eth5:c2_s1-eth5
+# c1_s2 lo:  c1_s2-eth1:c1_s1-eth2 c1_s2-eth2:c1_h3-eth0 c1_s2-eth3:c1_h4-eth0 c1_s2-eth4:c2_s2-eth4 c1_s2-eth5:c3_s1-eth6
+# c2_s1 lo:  c2_s1-eth1:c2_ap1-eth1 c2_s1-eth2:c2_s2-eth1 c2_s1-eth3:c2_h1-eth0 c2_s1-eth4:c2_h2-eth0 c2_s1-eth5:c1_s1-eth5 c2_s1-eth6:c3_s1-eth5
+# c2_s2 lo:  c2_s2-eth1:c2_s1-eth2 c2_s2-eth2:c2_h3-eth0 c2_s2-eth3:c2_h4-eth0 c2_s2-eth4:c1_s2-eth4 c2_s2-eth5:c3_s2-eth4
+# c3_s1 lo:  c3_s1-eth1:c3_ap1-eth1 c3_s1-eth2:c3_s2-eth1 c3_s1-eth3:c3_h1-eth0 c3_s1-eth4:c3_h2-eth0 c3_s1-eth5:c2_s1-eth6 c3_s1-eth6:c1_s2-eth5
+# c3_s2 lo:  c3_s2-eth1:c3_s1-eth2 c3_s2-eth2:c3_h3-eth0 c3_s2-eth3:c3_h4-eth0 c3_s2-eth4:c2_s2-eth5
+# c6653
+# c6654
+# c6655
+# sta1 sta1-wlan0:wifi
+# sta2 sta2-wlan0:wifi
+# sta3 sta3-wlan0:wifi
+# c1_ap1 lo:  c1_ap1-eth1:c1_s1-eth1 c1_ap1-wlan2:wifi c1_ap1-wlan3:wifi
+# c2_ap1 lo:  c2_ap1-eth1:c2_s1-eth1 c2_ap1-wlan2:wifi c2_ap1-wlan3:wifi
+# c3_ap1 lo:  c3_ap1-eth1:c3_s1-eth1 c3_ap1-wlan2:wifi c3_ap1-wlan3:wifi
+
+# Add flood rule to each (default action: flood all unmatched packets)
+# Use -O OpenFlow13 to ensure compatibility
+ovs-ofctl -O OpenFlow13 add-flow c1_s1 priority=0,actions=flood
+ovs-ofctl -O OpenFlow13 add-flow c1_s2 priority=0,actions=flood
+ovs-ofctl -O OpenFlow13 add-flow c1_ap1 priority=0,actions=flood
+
+ovs-ofctl -O OpenFlow13 add-flow c2_s1 priority=0,actions=flood
+ovs-ofctl -O OpenFlow13 add-flow c2_s2 priority=0,actions=flood
+ovs-ofctl -O OpenFlow13 add-flow c2_ap1 priority=0,actions=flood
+
+ovs-ofctl -O OpenFlow13 add-flow c3_s1 priority=0,actions=flood
+ovs-ofctl -O OpenFlow13 add-flow c3_s2 priority=0,actions=flood
+ovs-ofctl -O OpenFlow13 add-flow c3_ap1 priority=0,actions=flood
