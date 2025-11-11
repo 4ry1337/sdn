a0433d37850f5fde2215bd612d59bd82aaa4f593
Rakhat Yskak <yskak.rakhat@gmail.com>
2025-11-12 02:36:04 +0500

    [project] prefix utils

--- Project/openvis/src/shared/lib/utils.ts
+++ Project/openvis/src/shared/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const add_prefix = (prefix: string, value: string): string => {
+  return `${prefix}::${value}`;
+}
+
+export const remove_prefix = (prefix: string): string => {
+  const parts = prefix.split('::');
+  return parts.length > 1 ? parts[1] : prefix;
+}
